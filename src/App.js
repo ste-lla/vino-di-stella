30b2b7547475ff6fafa2b4eb04fb894ed1280d09
Stella <stelladev7@gmail.com>
2022-02-21 19:04:16 -0500

    deployed to Netlify, changed Title in index.html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
     <div className="">
        <Routes>
         <Route exact path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
       </Routes>
     </div>
   );
